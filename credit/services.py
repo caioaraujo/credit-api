7976c5fae9699cc8f7cac58cce383f281a397372
caioaraujo <c.felipe.araujo@gmail.com>
2020-07-06 19:05:45 -0300

    send services in tasks.py to services.py

--- credit/services.py
+++ credit/services.py
@@ -1,14 +1,20 @@
+import json
 from decimal import Decimal
+
+import requests
+from django.conf import settings
 
 from .exceptions import LoanException
 from .models import Loan
 
-MIN_AMOUNT_ALLOWED = Decimal('1000')
-MAX_AMOUNT_ALLOWED = Decimal('4000')
-REQUIRED_FIELD = 'Required field'
-
 
 class LoanService:
+    MAX_AMOUNT_ALLOWED = Decimal('4000')
+    MIN_AMOUNT_ALLOWED = Decimal('1000')
+    REQUIRED_FIELD = 'Required field'
+    RESULT_APPROVED = 'approved'
+    RESULT_REJECTED = 'rejected'
+    STATUS_COMPLETED = 'completed'
 
     def __init__(self):
         self.errors = []
@@ -32,30 +38,114 @@ class LoanService:
 
         return loan
 
-    def _validate_data(self, data):
-        name = data.get('name')
-        if not name:
-            self.errors.append({'name': REQUIRED_FIELD})
-        cpf = data.get('cpf')
-        if not cpf:
-            self.errors.append({'cpf': REQUIRED_FIELD})
-        birthdate = data.get('birthdate')
-        if not birthdate:
-            self.errors.append({'birthdate': REQUIRED_FIELD})
-        amount_asked = data.get('amount')
-        if not amount_asked:
-            self.errors.append({'amount': REQUIRED_FIELD})
-        elif Decimal(amount_asked) < MIN_AMOUNT_ALLOWED or MAX_AMOUNT_ALLOWED < Decimal(amount_asked):
-            self.errors.append({'amount': 'Out of limit allowed'})
-        terms_asked = data.get('terms')
-        if not terms_asked:
-            self.errors.append({'terms': REQUIRED_FIELD})
-        income = data.get('income')
-        if not income:
-            self.errors.append({'income': REQUIRED_FIELD})
-
     def get_result(self, loan_id):
         return Loan.objects.get(id=loan_id)
 
     def get_loans_in_process(self):
         return Loan.objects.filter(status='processing')
+
+    def refuse_loan(self, loan_id, refused_policy):
+        loan_obj = Loan.objects.get(id=loan_id)
+        loan_obj.result = self.RESULT_REJECTED
+        loan_obj.refused_policy = refused_policy
+        loan_obj.status = self.STATUS_COMPLETED
+        loan_obj.save(update_fields=['result', 'refused_policy', 'status'])
+
+    def approved_loan(self, loan_id, terms_approved, amount_approved):
+        loan_obj = Loan.objects.get(id=loan_id)
+        loan_obj.result = self.RESULT_APPROVED
+        loan_obj.status = self.STATUS_COMPLETED
+        loan_obj.terms_approved = terms_approved
+        loan_obj.amount_approved = amount_approved
+        loan_obj.save(update_fields=['result', 'status', 'terms_approved', 'amount_approved'])
+
+    def _validate_data(self, data):
+        name = data.get('name')
+        if not name:
+            self.errors.append({'name': self.REQUIRED_FIELD})
+        cpf = data.get('cpf')
+        if not cpf:
+            self.errors.append({'cpf': self.REQUIRED_FIELD})
+        birthdate = data.get('birthdate')
+        if not birthdate:
+            self.errors.append({'birthdate': self.REQUIRED_FIELD})
+        amount_asked = data.get('amount')
+        if not amount_asked:
+            self.errors.append({'amount': self.REQUIRED_FIELD})
+        elif Decimal(amount_asked) < self.MIN_AMOUNT_ALLOWED or self.MAX_AMOUNT_ALLOWED < Decimal(amount_asked):
+            self.errors.append({'amount': 'Out of limit allowed'})
+        terms_asked = data.get('terms')
+        if not terms_asked:
+            self.errors.append({'terms': self.REQUIRED_FIELD})
+        income = data.get('income')
+        if not income:
+            self.errors.append({'income': self.REQUIRED_FIELD})
+
+
+class CreditService:
+
+    def get_credit_score(self, cpf):
+        url = settings.SCORE_URL
+        access_token = settings.CREDIT_VALIDATION_TOKEN
+        response = requests.post(url, json={"cpf": cpf}, headers={'x-api-key': access_token})
+        response_dict = json.loads(response.text)
+
+        return response_dict["score"]
+
+    def get_commitment_pct(self, cpf):
+        url = settings.COMMITMENT_URL
+        access_token = settings.CREDIT_VALIDATION_TOKEN
+        response = requests.post(url, json={"cpf": cpf}, headers={'x-api-key': access_token})
+        response_dict = json.loads(response.text)
+
+        return Decimal(response_dict["commitment"])
+
+    def get_terms_approved(self, commitment_amount, amount_asked, income, current_term):
+        amount_per_term = Decimal(current_term / amount_asked)
+        if amount_per_term > commitment_amount:
+            if current_term == 6:
+                self.get_terms_approved(commitment_amount, amount_asked, income, 9)
+            elif current_term == 9:
+                self.get_terms_approved(commitment_amount, amount_asked, income, 12)
+            return 0
+
+        return current_term
+
+    def calculate_amount_approved(self, amount_asked, terms_approved):
+        interest_rate = self._calculate_interest_rate(amount_asked, terms_approved)
+
+        return amount_asked * (
+                ((1 + interest_rate) ^ terms_approved * interest_rate) / ((1 + interest_rate) ^ terms_approved - 1)
+        )
+
+    def _calculate_interest_rate(self, amount_asked, terms):
+        interest_rules = {
+            'MORE_THAN_900': {
+                6: Decimal('3.9'),
+                9: Decimal('4.2'),
+                12: Decimal('4.5'),
+            },
+            '800_TO_899': {
+                6: Decimal('4.7'),
+                9: Decimal('5.0'),
+                12: Decimal('5.3'),
+            },
+            '700_TO_799': {
+                6: Decimal('5.5'),
+                9: Decimal('5.8'),
+                12: Decimal('6.1'),
+            },
+            '600_TO_699': {
+                6: Decimal('6.4'),
+                9: Decimal('6.6'),
+                12: Decimal('6.9'),
+            }
+        }
+
+        if amount_asked > Decimal('900'):
+            return interest_rules['MORE_THAN_900'][terms]
+        if Decimal('800') <= amount_asked <= Decimal('899'):
+            return interest_rules['800_TO_899'][terms]
+        if Decimal('700') <= amount_asked <= Decimal('799'):
+            return interest_rules['700_TO_799'][terms]
+        return interest_rules['600_TO_699'][terms]
--- credit/tasks.py
+++ credit/tasks.py
@@ -1,24 +1,19 @@
 # Celery tasks file
-import json
 from datetime import datetime
 from decimal import Decimal
 
-import requests
 from celery import shared_task
-from django.conf import settings
 
-from .models import Loan
+from .services import CreditService, LoanService
 
-MIN_CREDIT_SCORE = 600
-RESULT_APPROVED = 'approved'
-RESULT_REJECTED = 'rejected'
+DATE_MASK = '%Y-%m-%d'
 STATUS_COMPLETED = 'completed'
 STATUS_PROCESSING = 'processing'
 
 
 @shared_task
 def validate_age(loan):
-    birthdate = datetime.strptime(loan['birthdate'], '%Y-%m-%d')
+    birthdate = datetime.strptime(loan['birthdate'], DATE_MASK)
     current_date = datetime.now()
     years_diff = current_date.year - birthdate.year
     if (
@@ -27,7 +22,7 @@ def validate_age(loan):
     ):
         years_diff = years_diff - 1
     if years_diff < 18:
-        refuse_loan(loan['id'], 'age')
+        LoanService().refuse_loan(loan['id'], 'age')
         return loan, STATUS_COMPLETED
 
     return loan, STATUS_PROCESSING
@@ -41,10 +36,11 @@ def validate_score(s, *args):
     if current_status == STATUS_COMPLETED:
         return loan, current_status
 
-    credit_score = get_credit_score(loan['cpf'])
+    credit_service = CreditService()
+    credit_score = credit_service.get_credit_score(loan['cpf'])
 
     if credit_score < 600:
-        refuse_loan(loan['id'], 'score')
+        LoanService().refuse_loan(loan['id'], 'score')
         return loan, STATUS_COMPLETED
 
     return loan, STATUS_PROCESSING
@@ -57,23 +53,21 @@ def validate_commitment(s, *args):
     if current_status == STATUS_COMPLETED:
         return loan, current_status
 
-    commitment_pct = get_commitment_pct(loan['cpf'])
+    credit_service = CreditService()
+    commitment_pct = credit_service.get_commitment_pct(loan['cpf'])
     amount_asked = Decimal(loan['amount_asked'])
     commitment_amount = Decimal(amount_asked - (amount_asked * commitment_pct))
-    terms_approved = get_terms_approved(commitment_amount, amount_asked, loan['income'], loan['terms_asked'])
+    terms_approved = credit_service.get_terms_approved(commitment_amount, amount_asked, loan['income'],
+                                                       loan['terms_asked'])
+    loan_service = LoanService()
 
     if terms_approved == 0:
-        refuse_loan(loan['id'], 'commitment')
+        loan_service.refuse_loan(loan['id'], 'commitment')
         return loan, STATUS_COMPLETED
 
-    amount_approved = calculate_amount_approved(amount_asked, terms_approved)
+    amount_approved = credit_service.calculate_amount_approved(amount_asked, terms_approved)
 
-    loan_obj = Loan.objects.get(id=loan['id'])
-    loan_obj.result = RESULT_APPROVED
-    loan_obj.status = STATUS_COMPLETED
-    loan_obj.terms_approved = terms_approved
-    loan_obj.amount_approved = amount_approved
-    loan_obj.save(update_fields=['result', 'status', 'terms_approved', 'amount_approved'])
+    loan_service.approved_loan(loan['id'], terms_approved, amount_approved)
 
 
 def run_credit_pipeline(loan):
@@ -83,82 +77,3 @@ def run_credit_pipeline(loan):
             validate_commitment.s()
     )
     credit_pipeline.apply_async()
-
-
-def get_credit_score(cpf):
-    url = settings.SCORE_URL
-    access_token = settings.CREDIT_VALIDATION_TOKEN
-    response = requests.post(url, json={"cpf": cpf}, headers={'x-api-key': access_token})
-    response_dict = json.loads(response.text)
-
-    return response_dict["score"]
-
-
-def get_commitment_pct(cpf):
-    url = settings.COMMITMENT_URL
-    access_token = settings.CREDIT_VALIDATION_TOKEN
-    response = requests.post(url, json={"cpf": cpf}, headers={'x-api-key': access_token})
-    response_dict = json.loads(response.text)
-
-    return Decimal(response_dict["commitment"])
-
-
-def get_terms_approved(commitment_amount, amount_asked, income, current_term):
-    amount_per_term = Decimal(current_term / amount_asked)
-    if amount_per_term > commitment_amount:
-        if current_term == 6:
-            get_terms_approved(commitment_amount, amount_asked, income, 9)
-        elif current_term == 9:
-            get_terms_approved(commitment_amount, amount_asked, income, 12)
-        return 0
-
-    return current_term
-
-
-def calculate_amount_approved(amount_asked, terms_approved):
-    interest_rate = calculate_interest_rate(amount_asked, terms_approved)
-
-    return amount_asked * (
-            ((1 + interest_rate) ^ terms_approved * interest_rate) / ((1 + interest_rate) ^ terms_approved - 1)
-    )
-
-
-def calculate_interest_rate(amount_asked, terms):
-    interest_rules = {
-        'MORE_THAN_900': {
-            6: Decimal('3.9'),
-            9: Decimal('4.2'),
-            12: Decimal('4.5'),
-        },
-        '800_TO_899': {
-            6: Decimal('4.7'),
-            9: Decimal('5.0'),
-            12: Decimal('5.3'),
-        },
-        '700_TO_799': {
-            6: Decimal('5.5'),
-            9: Decimal('5.8'),
-            12: Decimal('6.1'),
-        },
-        '600_TO_699': {
-            6: Decimal('6.4'),
-            9: Decimal('6.6'),
-            12: Decimal('6.9'),
-        }
-    }
-
-    if amount_asked > Decimal('900'):
-        return interest_rules["MORE_THAN_900"][terms]
-    if Decimal('800') <= amount_asked <= Decimal('899'):
-        return interest_rules['800_TO_899'][terms]
-    if Decimal('700') <= amount_asked <= Decimal('799'):
-        return interest_rules['700_TO_799'][terms]
-    return interest_rules['600_TO_699'][terms]
-
-
-def refuse_loan(loan_id, refused_policy):
-    loan_obj = Loan.objects.get(id=loan_id)
-    loan_obj.result = RESULT_REJECTED
-    loan_obj.refused_policy = refused_policy
-    loan_obj.status = STATUS_COMPLETED
-    loan_obj.save(update_fields=['result', 'refused_policy', 'status'])
